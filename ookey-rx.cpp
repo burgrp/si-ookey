c14d181d83fc61299037b36224c4af5ed99cfe5d
Pavel Burgr <pb@drake.cz>
2021-02-05 13:18:01 +0100

    WIP

--- ookey-rx.cpp
+++ ookey-rx.cpp
@@ -1,48 +1,37 @@
-namespace mark {
-
-int pin;
-
-void init(int gpioPin) {
-  pin = gpioPin;
-  target::PORT.DIRSET.setDIRSET(1 << pin);
-}
-
-void now(int length = 1) {
-  target::PORT.OUTSET.setOUTSET(1 << pin);
-  int wait = length * 8;
-  for (volatile int w = 0; w < wait; w++)
-    ;
-  target::PORT.OUTCLR.setOUTCLR(1 << pin);
-}
-
-} // namespace mark
-
 namespace ookey {
 namespace rx {
 
 class Decoder : public applicationEvents::EventHandler {
-  unsigned short address;
-  unsigned char buffer[2 + 1 + 256 + 2];
 
-  const int MIN_PREAMBLE_BITS = 20; // there is 31 ones sent by transmitter,
+  const int MIN_PREAMBLE_BITS = 20; // there are 31 ones sent by transmitter,
                                     // let's be tollerant to noisy start
+
+  union {
+    unsigned char buffer[2 + 1 + 256 + 2];
+    struct {
+      unsigned short address;
+      unsigned char length;
+      unsigned char data[256];
+    } their;
+  };
 
   int dataBits = -1;
   int preambleBits = 0;
   bool pendingData = false;
   int rxEventId;
 
-  // virtual void setTimerInterrupt(int time) = 0;
-  // virtual void setRfPinInterrupt(bool enabled) = 0;
-  virtual void dataReceived(unsigned char *data, int len) = 0;
+  virtual void dataReceived(unsigned short address, unsigned char *data,
+                            int len) = 0;
 
   void onEvent() {
-    dataReceived(&buffer[1], buffer[0]);
+    dataReceived(their.address, their.data, their.length);
     restart();
   }
 
 public:
-  
+  unsigned short address;
+  bool promisc = false;
+
   void init(unsigned short address) {
     this->address = address;
     rxEventId = applicationEvents::createEventId();
@@ -64,7 +53,7 @@ public:
       } else {
         if (preambleBits > MIN_PREAMBLE_BITS && !pendingData) {
           // ok, there was enough of preamble ones, let's start
-          dataBits = 0;          
+          dataBits = 0;
         } else {
           // preamble too short
           restart();
@@ -88,14 +77,18 @@ public:
         // complete byte
         if (bitIndex == 7) {
 
+          if (byteIndex == 1 && !promisc && their.address != address) {
+            restart();
+          }
+
           // are we complete incl. crc?
-          int len = buffer[2];
-          if (byteIndex > 2 && 2 + 1 + len + 2 == byteIndex + 1) {
+          if (byteIndex > 2 && 2 + 1 + their.length + 2 == byteIndex + 1) {
             int myCrc = 0x55;
-            for (int c = 0; c < len; c++) {
-              myCrc += buffer[c + 3];
+            for (int c = 0; c < their.length; c++) {
+              myCrc += their.data[c];
             }
-            int theirCrc = buffer[3 + len] | (buffer[3 + len + 1] << 8);
+            int theirCrc =
+                their.data[their.length] | (their.data[their.length + 1] << 8);
             if (theirCrc == myCrc) {
               pendingData = true;
               applicationEvents::schedule(rxEventId);
